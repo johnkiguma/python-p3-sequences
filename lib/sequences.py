dbd423c46016db6249ef67e73599e5afaf48b017
johnkiguma <john.kiguma@student.moringaschool.com>
2024-03-01 12:56:11 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    a, b = 0, 1
+    fibonacci_sequence = []
+    while len(fibonacci_sequence) < n:  # Modify condition to check length of sequence
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    print(fibonacci_sequence)
 
-def print_fibonacci(length):
-    pass
+print_fibonacci(88)
